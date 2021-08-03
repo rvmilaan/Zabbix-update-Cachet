638d472c5ff9fa88daae7bf302f1136b51f75e5f
Ruben van Milaan <46755234+rvmilaan@users.noreply.github.com>
2021-08-03 22:49:39 +0200

    Update CachetUpdate.sh
    comments updated

--- CachetUpdate.sh
+++ CachetUpdate.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-source requirments
+# source requirments (doesn't work with zabbix actions)
 # $1, $2 and $3 comming from Zabbix mail template
 #to=$1 (not used)
 subject=$2
 message=$3
 url=https://<YOUR-DOMAIN>/api/v1/components
 apitoken=<YOUR-API-KEY>
-# Uncomment this next line for Production
 curl  -H 'Content-Type: application/json' -H 'X-Cachet-Token: '$apitoken'' $url/${subject} -d '{"status": '$message'}' -X PUT
