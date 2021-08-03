3aea147f9c4bbb8623a28f965202f23e4b45f219
Ruben van Milaan <46755234+rvmilaan@users.noreply.github.com>
2021-08-03 22:43:59 +0200

    Update CachetUpdate.sh
    Update variable for Zabbix integration

--- CachetUpdate.sh
+++ CachetUpdate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#to=$1 (not in use)
+source requirments
+# $1, $2 and $3 comming from Zabbix mail template
+#to=$1 (not used)
 subject=$2
 message=$3
-
+url=https://status.xelionsystemen.com/api/v1/components
+apitoken=SgFYq9xlVRhPdC2yKDau
 # Uncomment this next line for Production
-curl -H 'Content-Type: application/json' -H 'X-Cachet-Token: SgFYq9xlVRhPdC2yKDau' https://status.xelionsystemen.com/api/v1/components/${subject} -d '{"status": '$message'}' -X PUT
+curl  -H 'Content-Type: application/json' -H 'X-Cachet-Token: '$apitoken'' $url/${subject} -d '{"status": '$message'}' -X PUT
